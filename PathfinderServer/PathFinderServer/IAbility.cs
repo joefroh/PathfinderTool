c32f98ef04e3a9fbc2a3d4c28c72aa20faeb6db9
Joey Frohlinger <jmafrohlinger@outlook.com>
2016-03-08 16:04:38 -0800

    Let the journey begin.

---
+++ PathfinderServer/PathFinderServer/IAbility.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PathFinderServer
+{
+    interface IAbility
+    {
+        int AbilityScore
+        {
+            get;
+        }
+
+        int AbilityModifier
+        {
+            get;
+        }
+
+        int TempAdjustment
+        {
+            get;
+        }
+
+        int TempModifier
+        {
+            get;
+        }
+    }
+}
