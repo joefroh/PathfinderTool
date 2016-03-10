595c782a0696d159091cb2821300ec12309ceaa0
Joey Frohlinger <jmafrohlinger@outlook.com>
2016-03-09 17:35:45 -0800

    Implemented abstract Skill class

---
+++ PathfinderServer/PathFinderServer/Skills/Skill.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PathFinderServer.Interfaces;
+
+namespace PathFinderServer.Skills
+{
+    abstract class Skill: ISkill
+    {
+        public abstract bool TrainedOnly { get; }
+        public abstract Abilities.Abilities AbilityBase { get; }
+        public abstract int AbilityMod { get; }
+        public int Ranks { get; set; }
+        public int MiscMod { get; set; }
+        public int TotalBonus()
+        {
+            return AbilityMod + Ranks + MiscMod;
+        }
+    }
+}
