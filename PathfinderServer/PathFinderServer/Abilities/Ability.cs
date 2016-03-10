208b135490b6e4f3d90ec5ef5874a2c5feb5706b
Joey Frohlinger <jmafrohlinger@outlook.com>
2016-03-09 17:11:24 -0800

    Started fleshing out Abilities data model

---
+++ PathfinderServer/PathFinderServer/Abilities/Ability.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PathFinderServer.Abilities
+{
+    internal abstract class Ability : IAbility
+    {
+        public int AbilityModifier
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public int AbilityScore
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public int TempAdjustment
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public int TempModifier
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
